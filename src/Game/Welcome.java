21f05bcc92e6a3628249ad9f22bbfdd8e139ea04
Ramya <Ramyasandra20@gmail.com>
2022-06-07 23:26:11 +0530

    I am uploading my project

---
+++ src/Game/Welcome.java
@@ -0,0 +1,12 @@
+package Game;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the world of Ramya's Game");
+		System.out.println("This is my first java project");
+
+	}
+
+}
